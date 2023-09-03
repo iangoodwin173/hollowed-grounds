2635e1fec4ec5616b6848c18119de9a660a463cb
Ian Goodwin <iangoodwin173@gmail.com>
2023-09-02 20:21:09 -0400

    added more content to new homepage

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const menuButtonShow = document.getElementById('menu-button-show');
+const menuButtonHide = document.getElementById('menu-button-hide');
+const navLinks = document.querySelector('.nav-links a');
+
+menuButtonShow.addEventListener('click', () => {
+   
+    console.log('button pressed');
+
+});
+
+menuButtonHide.addEventListener('click', () => {
+    
+    console.log('button pressed');
+
+});
